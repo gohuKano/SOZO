3aa1b036758bd9792e74e2412bc1815675cade5e
gohuKano <129407202+gohuKano@users.noreply.github.com>
2023-03-31 00:01:47 +0200

    old

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-# TODO: ajouter la possibilité de zoomer lorsque que 
+# TODO: 
+# ajouter la possibilité de zoomer lorsque que 
 # l'user passe la sourie sur l'image
-# créer un onglet de connexion
+# 
+# créer un onglet de connexion...
+# 
+# Finir l'affichage du texte pour la troisieme image
+#
+# Changer le nom des ul li en français
+
 
 from flask import Flask, render_template
 
