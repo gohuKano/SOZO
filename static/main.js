bb444f34407fc0d80f70c514a9914bc5410506ac
gohuKano <129407202+gohuKano@users.noreply.github.com>
2023-03-31 13:55:06 +0200

    oldest-v

--- static/main.js
+++ static/main.js
@@ -11,11 +11,11 @@ const form_client = document.getElementById("form-client-div");
 compte_button_show.addEventListener("click", () => {
 
     form_client.style.top = "0vh";
-    form_client.style.transition = "top 0.7s ease-in-out";
+    form_client.style.transition = "top 1s cubic-bezier(.12,1.01,.62,.96)";
 });
 
 compte_button_hide.addEventListener("click", () => {
 
     form_client.style.top = "-60vh";
-    form_client.style.transition = "top 0.7s ease-in-out";
+    form_client.style.transition = "top 1s cubic-bezier(.12,1.01,.62,.96)";
 });
