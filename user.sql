843db604a0df4510b4e450e14941edbae04b1020
gohuKano <129407202+gohuKano@users.noreply.github.com>
2023-04-01 19:42:57 +0200

    Update user.sql

--- user.sql
+++ user.sql
@@ -7,3 +7,18 @@ CREATE TABLE accounts (
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
     password VARCHAR ( 50 ) NOT NULL
 );
+
+CREATE TABLE products (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	description TEXT,
+	price DECIMAL(10,2) NOT NULL,
+	image_url VARCHAR(255),
+	category VARCHAR(255),
+	gender VARCHAR(255),
+	size VARCHAR(255),
+	color VARCHAR(255),
+	material VARCHAR(255),
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
