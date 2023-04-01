8e700d82271a3a42bf7975edd45d08e91acc25ab
gohuKano <129407202+gohuKano@users.noreply.github.com>
2023-04-01 20:04:06 +0200

    oldest-v

--- user.sql
+++ user.sql
@@ -1,9 +1,7 @@
 CREATE TABLE accounts (
 	user_id serial PRIMARY KEY,
-
     prenom VARCHAR ( 50 ) NOT NULL,
 	nom VARCHAR ( 50 ) NOT NULL,
-    
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
     password VARCHAR ( 50 ) NOT NULL
 );
@@ -12,13 +10,18 @@ CREATE TABLE products (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
 	description TEXT,
-	price DECIMAL(10,2) NOT NULL,
-	image_url VARCHAR(255),
+	price_HT DECIMAL(10,2) NOT NULL,
 	category VARCHAR(255),
 	gender VARCHAR(255),
 	size VARCHAR(255),
-	color VARCHAR(255),
-	material VARCHAR(255),
-	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+	color VARCHAR(255)
 );
+
+CREATE TABLE cart (
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER REFERENCES client(id),
+    product_id INTEGER REFERENCES product(id),
+    quantity INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
