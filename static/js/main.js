69a1701929653faf488bd31017ce131e16189ba8
gohuKano <129407202+gohuKano@users.noreply.github.com>
2023-04-05 00:48:58 +0200

    V2

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,15 @@ const login_box = document.getElementById("login-box");
 const login_box_blur = document.getElementById("login-box-blur");
 const to_blur = document.querySelectorAll(".to-blur");
 
-function openForm() {
-    for (let i = 0; i < to_blur.length; i++) {
-        to_blur[i].style.filter = "blur(1.5px)";
-    }
+const enable_Form = (e) => {
+    document.getElementById('login-box').classList.toggle("show");
 
-    login_box_blur.style.display = "block";
-    login_box_blur.style.opacity = 0.6;
+    // for (let i = 0; i < to_blur.length; i++) {
+    //     to_blur[i].style.filter = "blur(1.5px)";
+    // }
+
+    // login_box_blur.style.display = "block";
+    // login_box_blur.style.opacity = 0.6;
     
 
 
@@ -30,21 +32,23 @@ function openForm() {
     login_box.style.top = loginBoxPosY + 'px';
 
     // Montrez la boîte de dialogue
-    login_box.style.display = "block";
-    login_box.style.opacity = 0;
-    let opacity = 0;
+    // login_box.style.display = "block";
+    // login_box.style.opacity = 0;
+    // let opacity = 0;
 
-    const interval = setInterval(function() {
-        opacity += 0.05;
-        login_box.style.opacity = opacity;
-        if (opacity >= 1) {
-            clearInterval(interval);
-        }
-    }, 10);
+
+    // to change :
+    // const interval = setInterval(function() {
+    //     opacity += 0.05;
+    //     login_box.style.opacity = opacity;
+    //     if (opacity >= 1) {
+    //         clearInterval(interval);
+    //     }
+    // }, 10);
 
     // Empêchez le défilement de la page
     document.documentElement.style.overflow = 'hidden';
-    }
+}
     
 
 document.addEventListener('click', function(event) {
@@ -68,7 +72,6 @@ document.addEventListener('click', function(event) {
                 login_box.style.display = "none";
                 login_box_blur.style.display = "none";
                 login_box_blur.style.opacity = 0;
-                document.body.style.filter = "blur(0px)";
 
                 clearInterval(interval);
             }
@@ -77,7 +80,8 @@ document.addEventListener('click', function(event) {
 
 });
 
-const enable = (e) => {
+// to open responsive menu ( big burger mmmhhhhhhhhhhhhh )
+const enable_burger = (e) => {
     document.getElementById('burger').classList.toggle("open");
     document.getElementById("mobile-only__nav").classList.toggle("mobile-only__nav-open");
 };
