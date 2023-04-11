c70363c98d8a7b7162d1e272a99195abfa63758f
gohuKano <129407202+gohuKano@users.noreply.github.com>
2023-04-11 17:52:09 +0200

    V2

--- static/js/register.js
+++ static/js/register.js
@@ -1,22 +1,46 @@
-// get value for email
 const emailInput = document.querySelector('input[name="email"]');
 const confirmEmailInput = document.querySelector('input[name="confirm-email"]');
 const borderEmail = document.getElementById("border-email")
 const borderConfirmEmail = document.getElementById("border-confirm-email");
-
-// get value for password
 const passwordInput = document.querySelector('input[name="password-register"]');
 const confirmPasswordInput = document.querySelector('input[name="confirm-password"]');
 const borderPassword = document.getElementById("border-password")
 const borderConfirmPassword = document.getElementById("border-confirm-password");
+const img_show = document.getElementById("register__box__img--show");
+const img_hide = document.getElementById("register__box__img--hide");
+const img_show_2 = document.getElementById("register__box__img_2--show");
+const img_hide_2 = document.getElementById("register__box__img_2--hide");
+const passwordDialog = document.getElementById("password-dialog");
+
+emailInput.addEventListener('input', function() {
+  const email = emailInput.value;
+  const confirmEmail = confirmEmailInput.value;
+
+  if (email !== confirmEmail && email != ""){
+    // #c0392b = rouge
+    borderConfirmEmail.style.borderColor = "#c0392b";
+    borderEmail.style.borderColor = "#c0392b";
+  } else if (email == ""){
+    borderConfirmEmail.style.borderColor = "#000";
+    borderEmail.style.borderColor = "#000";
+  } else if (email === confirmEmail) {
+    borderConfirmEmail.style.borderColor = "#27ae60";
+    borderEmail.style.borderColor = "#27ae60";
+  }
+});
 
 confirmEmailInput.addEventListener('input', function() {
   const email = emailInput.value;
   const confirmEmail = confirmEmailInput.value;
-  if (email !== confirmEmail){
+
+  if (email !== confirmEmail && email != ""){
+    // #c0392b = rouge
     borderConfirmEmail.style.borderColor = "#c0392b";
     borderEmail.style.borderColor = "#c0392b";
-  } else {
+  } else if (email == ""){
+    borderConfirmEmail.style.borderColor = "#000";
+    borderEmail.style.borderColor = "#000";
+  } else if (email === confirmEmail) {
     borderConfirmEmail.style.borderColor = "#27ae60";
     borderEmail.style.borderColor = "#27ae60";
   }
@@ -25,12 +49,28 @@ confirmEmailInput.addEventListener('input', function() {
 confirmPasswordInput.addEventListener('input', function() {
   const password = passwordInput.value;
   const confirmPassword = confirmPasswordInput.value;
-  if (password !== confirmPassword){
+
+  const char_speciaux = /[!@#$%^&*(),.?":{}|<>]/; // caractères spéciaux autorisés
+  const chiffres = /[0-9]/; // chiffres autorisés
+  const minuscules = /[a-z]/; // minuscules autorisées
+  const majuscules = /[A-Z]/; // majuscules autorisées
+
+  const hasCharSpeciaux = char_speciaux.test(password);
+  const hasChiffres = chiffres.test(password);
+  const hasMinuscules = minuscules.test(password);
+  const hasMajuscules = majuscules.test(password);
+  const hasMinLength = password.length >= 8;
+
+  if (password != confirmPassword){
     borderConfirmPassword.style.borderColor = "#c0392b";
     borderPassword.style.borderColor = "#c0392b";
-  } else {
+  } else if (hasCharSpeciaux && hasChiffres && hasMinuscules && hasMajuscules && hasMinLength) {
     borderConfirmPassword.style.borderColor = "#27ae60";
     borderPassword.style.borderColor = "#27ae60";
+  }
+  if (password == "") {
+    borderPassword.style.borderColor = "#000";
+    borderConfirmPassword.style.borderColor = "#000";
   }
 });
 
@@ -61,9 +101,11 @@ passwordInput.addEventListener('input', function() {
     } else {
         borderPassword.style.borderColor = "red";
     }
+    if (password == "") {
+      borderPassword.style.borderColor = "#000";
+      borderConfirmPassword.style.borderColor = "#000";
+    }
 });
-
-const passwordDialog = document.getElementById("password-dialog");
 
 passwordInput.addEventListener('focus', function() {
   // Afficher la boîte de dialogue avec les conditions
@@ -80,9 +122,6 @@ passwordInput.addEventListener('blur', function() {
   passwordDialog.style.display = "none";
 });
 
-const img_show = document.getElementById("register__box__img--show");
-const img_hide = document.getElementById("register__box__img--hide");
-
 const show_psw = (e) => {
   img_hide.classList.add("show");
   img_show.classList.add("show");
@@ -93,4 +132,16 @@ const hide_psw = (e) => {
   document.getElementById('register__box__img--hide').classList.remove("show");
   document.getElementById("register__box__img--show").classList.remove("show");
   passwordInput.type = "password";
+};
+
+const show_psw_2 = (e) => {
+  img_hide_2.classList.add("show");
+  img_show_2.classList.add("show");
+  confirmPasswordInput.type = "text";
+};
+
+const hide_psw_2 = (e) => {
+  document.getElementById('register__box__img_2--hide').classList.remove("show");
+  document.getElementById("register__box__img_2--show").classList.remove("show");
+  confirmPasswordInput.type = "password";
 };
